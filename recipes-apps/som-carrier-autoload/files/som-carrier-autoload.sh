d03a97b4beb8ccdbb44be6f3c15fe524cba8b9f7
Swagath Gadde <swagath.gadde@xilinx.com>
2021-02-09 16:30:47 +0530

    som-carrier-autoload:add som-carrier-autoload recipe
    This patch will add som-carrier-autoload recipe in meta-som layer.
    when enabled it will autoload the respective carrier dtbo by reading eeprom data.
    
    Signed-off-by: Swagath Gadde <swagath.gadde@xilinx.com>
    Signed-off-by: Sai Hari Chandana Kalluri <chandana.kalluri@xilinx.com>

---
+++ recipes-apps/som-carrier-autoload/files/som-carrier-autoload.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+manufacturer=$(fru-print.py -b cc -f manufacturer)
+som_name=$(fru-print.py -b som -f product)
+carrier_card=$(fru-print.py -b cc -f product | tr '[:upper:]' '[:lower:]')
+cc_rev=$(fru-print.py -b cc -f revision)
+cc_rev_ver=$(printf "%02d" $(fru-print.py -b cc -f common version))
+
+dtbo_path="/boot/devicetree/"
+dtbo_file="zynqmp-$carrier_card-rev$cc_rev$cc_rev_ver.dtbo"
+
+echo "SOM:$som_name CARRIER_CARD:$carrier_card REVISION:$cc_rev VERSION:$cc_rev_ver"
+if [ -f ${dtbo_path}/$dtbo_file ]; then
+	overlay_path="/configfs/device-tree/overlays/"
+	echo "Applying $dtbo_file"
+	[ ! -d "/configfs" ] && mkdir /configfs
+	mount -t configfs configfs /configfs
+	cd $overlay_path
+	[ ! -d "./full" ] && mkdir full
+	[ ! -d "/lib/firmware" ] && mkdir /lib/firmware/
+	cp ${dtbo_path}/$dtbo_file /lib/firmware/.
+	echo -n "$dtbo_file" > full/path
+else
+	echo "NO CARRIER DTBO FOUND, PLEASE CHECK $dtbo_path"
+fi
