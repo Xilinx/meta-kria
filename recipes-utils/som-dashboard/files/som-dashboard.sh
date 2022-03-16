fd081cd8e043991fa35c39b1c9960c4b9345437c
Jue <jue.sun@xilinx.com>
2022-03-16 15:31:49 -0700

    update som-dashboard.sh for when addr isnt present
    update som-dashboard.sh so it doesnt start som-dashboard if IP address is not ready, and print out error message to let user how to call the script once IP addr is there.
    
    Signed-off-by: Jue <jue.sun@xilinx.com>
    Signed-off-by: Mark Hatle <mark.hatle@xilinx.com>

--- recipes-utils/som-dashboard/files/som-dashboard.sh
+++ recipes-utils/som-dashboard/files/som-dashboard.sh
@@ -27,5 +27,11 @@
 ip=$(ip -4 addr show eth0 | grep -oE "inet ([0-9]{1,3}[\.]){3}[0-9]{1,3}" | cut -d ' ' -f2)
 python_path=$(python3 -m site | grep packages |grep usr | sed 's/,//g' | sed 's/ //g'| sed 's/^.//;s/.$//')
 
-echo "SOM Dashboard will be running at http://$ip:5006/som-dashboard"
-bokeh serve --show --allow-websocket-origin=$ip:5006 $python_path/som-dashboard
+if [ -z $ip ]; then
+     echo "Cant find IP addr, please call /usr/bin/som-dashboard.sh after assigning IP addr"
+     exit 1
+else
+    echo "SOM Dashboard will be running at http://$ip:5006/som-dashboard"
+    bokeh serve --show --allow-websocket-origin=$ip:5006 $python_path/som-dashboard
+fi
+
