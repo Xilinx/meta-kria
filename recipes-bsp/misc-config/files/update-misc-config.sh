bd99a0aa23a7f4f2da0c45866f8697a26291d417
Ravitheja Varma Dakshiraju <ravitheja.varma.dakshiraju@xilinx.com>
2022-03-29 18:13:42 +0530

    misc-config:Update script to assign MAC address for eth1
    This patch will update the script to update MAC address for eth1 by reading FRU data using fru-print application
    
    Signed-off-by: Ravitheja Varma Dakshiraju <ravitheja.varma.dakshiraju@xilinx.com>
    Signed-off-by: Mark Hatle <mark.hatle@xilinx.com>
    (cherry picked from commit 5fac4bc6511cf258e60d78b2bc766870809e45f4)
    Signed-off-by: Mark Hatle <mark.hatle@xilinx.com>

--- recipes-bsp/misc-config/files/update-misc-config.sh
+++ recipes-bsp/misc-config/files/update-misc-config.sh
@@ -1,36 +1,46 @@
 #!/bin/sh                                                                       
-                                                                                
-function Get_MAC_ID(){                                                     
-         local output=""                                                        
-         output="$(fru-print.py -b som -f multirecord MAC_Addr MAC_ID_0)" 
-         echo $output                 
-}                                     
-                                      
-function Get_Active_Ethernet(){           
-                                      
-	local output=""                       
-	output=$(ls /sys/class/net | grep eth)
-                                 
-	if [ -z "$output" ]              
-	then                                     
-        	echo "No Ethernet Port Found"
-		exit 1	
-	else                                     
-        	line=${output:0:4}                    
-        	echo "$line"                          
-	fi                                                         
-} 
+  
+function Get_MAC_ID(){
 
+	local output=""
+	local index=$1
+	if [ $index -eq 0 ]; then
+		output="$(fru-print.py -b som -f multirecord MAC_Addr MAC_ID_0)"
+	else
+		output="$(fru-print.py -b cc -f multirecord MAC_Addr PS_MAC_ID_$index)"
+	fi
+	echo $output                 
+}
+
+function Get_Active_Ethernet(){
+
+	local output=""
+	local num=$1
+	output=$(ls /sys/class/net | grep eth$num)
+
+	if [ -z "$output" ]; then
+		echo "No Ethernet Port Found"
+		exit 1
+	else
+		line=${output:0:4}
+		echo "$line"                          
+	fi
+}
 
 function Update_MAC_Address(){
 
-        local MAC_ID=$(Get_MAC_ID)
-        local eth=$(Get_Active_Ethernet)
-        /sbin/ifconfig $eth down
-        /sbin/ifconfig $eth hw ether $MAC_ID
-        /sbin/ifconfig $eth up
-	local MAC_addr=$(cat /sys/class/net/$eth/address)
-        echo "MAC address for $eth is updated to $MAC_addr"
+	i=$(ls -ld /sys/class/net/eth* | wc -l)
+	while [ $i -gt 0 ]; do
+		i=$(( i - 1 ))
+		local eth=$(Get_Active_Ethernet $i)
+		local MAC_ID=$(Get_MAC_ID $i)
+		/sbin/ifconfig $eth down
+		/sbin/ifconfig $eth hw ether $MAC_ID
+		/sbin/ifconfig $eth up
+		local MAC_addr=$(cat /sys/class/net/$eth/address)
+		echo "MAC address for $eth is updated to $MAC_addr"
+	done
 }
 
 Update_MAC_Address
+
