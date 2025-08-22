f9a925ad52b9c70286989669b5aabb7e01f27490
Mark Hatle <mark.hatle@amd.com>
2025-08-22 13:12:54 -0600

    scripts: Use machineyaml for machine generation
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>

---
+++ scripts/generate-machines.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+# Use the machineyaml file to refresh the machine
+#
+# Usage: generate-machines.sh [<layer>] [<machine>]
+
+if [ -n "$1" ]; then
+    layer=$(realpath $1)
+    if [ ! -d $layer ]; then
+        echo "ERROR: The specified layer path does not exist" >&2
+        exit 1
+    fi
+else
+    layer=$(dirname $0)/..
+    layer=$(realpath $layer)
+fi
+
+if [ ! -d $layer/conf/machineyaml ]; then
+    echo "ERROR: The layer does not contain a conf/machineyaml directory."
+    exit 1
+fi
+
+gmc=`which gen-machineconf`
+if [ -z "${gmc}" ]; then
+  echo "ERROR: This script must be run in a configured Yocto Project build with gen-machineconf in the environment." >&2
+  exit 1
+fi
+
+gen_machine() {
+    machine=$1
+    if [ ! -e $layer/conf/machineyaml/$machine.yaml ]; then
+        echo "ERROR: Unable to find machine $machine in $layer/conf/machineyaml" >&2
+        exit 1
+    fi
+    machineyaml=$layer/conf/machineyaml/$machine.yaml
+
+    echo
+    echo machine: $machine
+    echo machineyaml: $machineyaml
+    echo ${gmc} --template $machineyaml -c $layer/conf
+    ${gmc} --template $machineyaml -c $layer/conf
+
+    # Cleanup tmp files
+    rm -rf output hw-description
+}
+
+cd $layer/conf/machineyaml
+if [ -n "$2" ] ; then
+    gen_machine $2
+else
+    for machineyaml in * ; do
+        gen_machine ${machineyaml/.yaml/}
+    done
+fi
---
+++ scripts/update-machineyaml.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+# Update the hw-description url in the machineyaml files against an index
+#
+# Usage: update-machineyaml.sh <index> [<layer>] [<machine>]
+
+if [ -z "$1" ]; then
+    echo "ERROR: Must specify the path to an index file" >&2
+    exit 1
+fi
+index=$(realpath $1)
+
+if [ -n "$2" ]; then
+    layer=$(realpath $2)
+    if [ ! -d $layer ]; then
+        echo "ERROR: The specified layer path does not exist" >&2
+        exit 1
+    fi
+else
+    layer=$(dirname $0)/..
+    layer=$(realpath $layer)
+fi
+
+if [ ! -d $layer/conf/machineyaml ]; then
+    echo "ERROR: The layer does not contain a conf/machineyaml directory."
+    exit 1
+fi
+
+update_yaml() {
+    machine=$1
+    if [ ! -e $layer/conf/machineyaml/$machine.yaml ]; then
+        echo "ERROR: Unable to find machine $machine in $layer/conf/machineyaml" >&2
+        exit 1
+    fi
+    machineyaml=$layer/conf/machineyaml/$machine.yaml
+
+    echo
+    echo machineyaml: $layer/conf/machineyaml/$machineyaml
+    echo machine: $machine
+
+    hwurl=$(grep '^'$machine' ' $index | cut -d ' ' -f 2)
+    if [ -z "$hwurl" ]; then
+        echo "WARNING: Unable to find $machine in the index, skipping..." >&2
+        continue
+    fi
+
+    echo "url: $hwurl"
+    sed -i $machineyaml -e 's,--hw-description .*,--hw-description '$hwurl','
+}
+
+cd $layer/conf/machineyaml
+if [ -n "$3" ]; then
+    update_yaml $3
+else
+    for machineyaml in * ; do
+        update_yaml ${machineyaml/.yaml/}
+    done
+fi
