13a41f3d3f4b5c077e44afde17ba0cd98a0ff834
Mark Hatle <mark.hatle@amd.com>
2025-10-28 16:30:49 -0600

    scripts: Remove need for 2025.2-sdt-index
    The index file is now optional.  It has been removed by default to avoid
    obsolete information being used to regenerate machine files.  The scripts
    have been modified to match and shell error checking has been introduced.
    
    Signed-off-by: Mark Hatle <mark.hatle@amd.com>
    Message-Id: <20251028223049.713333-8-mark.hatle@amd.com>
    Signed-off-by: Sandeep Gundlupet Raju <sandeep.gundlupet-raju@amd.com>

--- scripts/generate-machines-sdt.sh
+++ scripts/generate-machines-sdt.sh
@@ -1,20 +1,24 @@
 #! /bin/bash -e
 
 # Compatibility script for anyone still using generate-machines-sdt.sh
+# $0: <conf_path> <machine_url_index> [machine]
 
 this=$(realpath $0)
-
-if [ $# -lt 2 ]; then
-  echo "$0: <conf_path> <machine_url_index> [machine]" >&2
-  exit 1
+if [ -n "$1" ]; then
+  layer_path=$(realpath $1/..)
+else
+  layer_path=$(dirname $this)
+  layer_path=$(dirname $layer_path)
 fi
 
-echo
-echo INFO: Updating machineyaml URLs...
-echo
-$(dirname $this)/update-machineyaml.sh $2 $(realpath $1/..) $3
+if [ -n "$2" ]; then
+  echo
+  echo INFO: Updating machineyaml URLs...
+  echo
+  $(dirname $this)/update-machineyaml.sh $2 $layer_path $3
+fi
 
 echo
 echo INFO: Generating new machines...
 echo
-$(dirname $this)/generate-machines.sh $(realpath $1/..) $3
+$(dirname $this)/generate-machines.sh $layer_path $3
